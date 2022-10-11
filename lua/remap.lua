b6cc6d4f449334599eb5a99fb8969cce9a0f1402
Leonardo EscuerRuiz <leonardo.escuer@drip.com>
2022-10-11 21:15:24 +0200

    new keybinds

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,3 +1,5 @@
 local nnoremap = require("keymap").nnoremap
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap( "<C-p>", "<cmd>FzfLua files<CR>") 
+nnoremap("<C-h>","<cmd>tabprevious<CR>")
+nnoremap("<C-l>", "<cmd>tabnext<CR>")
