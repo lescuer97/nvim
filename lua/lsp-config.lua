a430efec51714b1cfdc66b1bbdfa32715bf8bfe3
Leonardo EscuerRuiz <leonardo.escuer@drip.com>
2022-11-08 16:49:57 +0100

    reset rust-analyzer

--- lua/lsp-config.lua
+++ lua/lsp-config.lua
@@ -29,6 +29,20 @@ debug = false, -- enable debug logging for commands
 go_to_source_definition = {
     fallback = true, -- fall back to standard LSP definition on failure
 },
+ server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+},
 }
 require('rust-tools').setup(opts)
 require("typescript").setup(opts)
