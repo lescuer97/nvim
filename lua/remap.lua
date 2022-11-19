5d912176ba1f00390a398c7b50b8ff3550fa4402
leonardo <leoescuer@protonmail.com>
2022-11-19 14:20:02 +0100

    add Include flag to search

--- lua/remap.lua
+++ lua/remap.lua
@@ -2,7 +2,7 @@ local nnoremap = require("keymap").nnoremap
 local FzfLua = require("fzf-lua");
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
-nnoremap( "<C-p>",  "<cmd>lua require('fzf-lua').files({ cmd = 'fdfind --exclude build --exclude node_modules --exclude archive' })<CR>") 
+nnoremap( "<C-p>",  "<cmd>lua require('fzf-lua').files({ cmd = 'fdfind -I --exclude build/ --exclude node_modules/ --exclude archive/' })<CR>") 
 nnoremap( "<C-g>",  "<cmd>lua require('fzf-lua').live_grep_glob({filter = [[rg --invert-match 'node_modules/|build/|archive/|dist/|.git/|Cargo.lock|package-lock.json|LICENSES.txt|LICENSES.json']]})<CR>") 
 nnoremap("<C-h>","<cmd>tabprevious<CR>")
 nnoremap("<C-l>", "<cmd>tabnext<CR>")
